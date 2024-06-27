389bcf0304e52b8b9ac9fb2c16dfa4e84f74efe3
Bob Park <hwpark11@kakao.com>
2024-06-27 21:50:13 +0900

    feat: 먼가 수정

--- src/app/video/_component/VideoContents.tsx
+++ src/app/video/_component/VideoContents.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import VideoPlayer from "@/components/VideoPlayer";
+import VideoPlayer from '@/components/VideoPlayer';
 
 const DEFAULT_FPS = 29.97;
 
@@ -11,42 +11,5 @@ function parseFrameCount(currentTime: number, currentFps?: number) {
 }
 
 export default function VideoContents() {
-
-    const
-
-  return (
-    <div>
-      <div className="">
-        <VideoPlayer src="/api/dash/test.mpd" />
-      </div>
-      <div>
-        <div className="flex flex-row items-center justify-center">
-          <span className="mr-3 counter">
-            {Math.floor(parseFrameCount(currentTime))}
-          </span>
-          /<span className="ml-3">{Math.floor(parseFrameCount(duration))}</span>
-        </div>
-      </div>
-      {/* <div>
-        <div className="flex flex-row items-center justify-center">
-          <span className="mr-3 counter">
-            {toTimecode(currentFrameCount, FPS, true)}
-          </span>
-          /
-          <span className="ml-3">{toTimecode(totalFrameCount, FPS, true)}</span>
-        </div>
-      </div>
-      <div>
-        <div className="flex flex-row items-center justify-center">
-          <span className="mr-3 counter">
-            {toTimecode(currentFrameCount, FPS, false)}
-          </span>
-          /
-          <span className="ml-3">
-            {toTimecode(totalFrameCount, FPS, false)}
-          </span>
-        </div>
-      </div> */}
-    </div>
-  );
+  return <div></div>;
 }
